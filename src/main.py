9262a1d5ccf01e15f875bc6cd1ee03523461e90c
redanaser <171096342+redanaser@users.noreply.github.com>
2025-07-07 18:26:06 +0300

    adding the factoory to the project lifespan

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from routes import base, data
-
+from stores.llm.LLMProviderFactory import LLMProviderFactory
 
 #The method "on_event" in class "FastAPI" is deprecated
 #on_event is deprecated, use lifespan event handlers instead.
@@ -15,6 +15,17 @@ async def lifespan(app: FastAPI):
     app.mongo_conn = mongo_conn
     app.db_client = mongo_conn[settings.MONGODB_DATABASE]
     
+    llm_provider_factory = LLMProviderFactory(settings)
+
+    # generation client
+    app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(model_id = settings.GENERATION_MODEL_ID)
+
+    # embedding client
+    app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
+                                             embedding_size=settings.EMBEDDING_MODEL_SIZE)
+  
     yield  # App is running
 
     mongo_conn.close()  # On shutdown
