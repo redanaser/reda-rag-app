61ea34a27dc02ba7fda205237c81754659dc0e6d
redanaser <171096342+redanaser@users.noreply.github.com>
2025-07-07 18:26:17 +0300

    updating openai

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -8,6 +8,21 @@ class Settings(BaseSettings):
     FILE_DEFAULT_CHUNK_SIZE : int
     MONGODB_URL : str
     MONGODB_DATABASE : str
+
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str = None
+    OPENAI_API_URL: str = None
+    COHERE_API_KEY: str = None
+
+    GENERATION_MODEL_ID: str = None
+    EMBEDDING_MODEL_ID: str = None
+    EMBEDDING_MODEL_SIZE: int = None
+    INPUT_DAFAULT_MAX_CHARACTERS: int = None
+    GENERATION_DAFAULT_MAX_TOKENS: int = None
+    GENERATION_DAFAULT_TEMPERATURE: float = None
+
     class Config:
         env_file=".env"
     
