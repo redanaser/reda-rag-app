9eea96e231e409bcf95d2332ad353214d2886dbf
redanaser <171096342+redanaser@users.noreply.github.com>
2026-05-22 18:44:20 +0200

    add pgvector enums

--- src/stores/vectordb/VectorDBEnums.py
+++ src/stores/vectordb/VectorDBEnums.py
@@ -2,7 +2,24 @@ from enum import Enum
 
 class VectorDBEnums(Enum):
     QDRANT = "QDRANT"
+    PGVECTOR= "PGVECTOR"
 
 class DistanceMethodEnums(Enum):
     COSINE = "cosine"
     DOT = "dot"
+
+class PgVectorTableSchemeEnums(Enum):
+    ID = "id"
+    TEXT = "text" # optional, can be used to store the original text or a reference to it
+    VECTOR = "vector"
+    CHUNKID = "chunk_id"
+    METADATA = "metadata"
+    _PREFIX = "pgvector"
+
+class PgVectorDistanceMethodEnums(Enum):
+    COSINE = "vector_cosine_ops"
+    DOT = "vector_l2_ops"
+
+class PgVectorIndexTypeEnums(Enum):
+    IVFFLAT = "IVFFLAT" # Default, greedy.
+    HNSW = "HNSW" # Hierarchical Navigable Small World graphs
